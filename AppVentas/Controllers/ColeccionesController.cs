0a118072135f78708eea4853367fa48943c52fa5
wurupio <131417911+wurupio@users.noreply.github.com>
2024-06-06 01:58:36 -0500

    Corregir Header

---
+++ AppVentas/Controllers/ColeccionesController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppVentas.Controllers
+{
+    public class ColeccionesController : Controller
+    {
+        // GET: Colecciones
+        public ActionResult Summer()
+        {
+            return View();
+        }
+        public ActionResult Scape()
+        {
+            return View();
+        }
+        public ActionResult Line()
+        {
+            return View();
+        }
+
+        // GET: Colecciones/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Colecciones/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Colecciones/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Colecciones/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Colecciones/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Colecciones/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Colecciones/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
